f3e63c85a6dd0dd044308380d961b4067dd368dd
Rubén Pérez <rubenpo167@gmail.com>
2020-10-05 10:46:07 +0200

    validaciones ok

--- src/app/components/post-form/post.form.component.ts
+++ src/app/components/post-form/post.form.component.ts
@@ -18,68 +18,75 @@ export class PostFormComponent {
   public telefono: FormControl;
   public numbers = [];
   public generos = ["Hombre", "Mujer"];
+
   constructor(private formBuilder: FormBuilder) {
     this.numbers = Array(100)
       .fill(Number)
       .map((x, i) => i + 1)
       .slice(9);
 
-    this.personalDataForm = this.formBuilder.group({
-      nombre: ["", [Validators.required]],
-      apellidos: ["", [Validators.required]],
-      edad: ["", [Validators.required, this.ageValidator]],
-      sexo: ["", [Validators.required]],
-      DNI: [
-        "",
-        [Validators.required, Validators.pattern(/((\d{8})([-]?)([a-zA-Z]))/)],
-      ],
-      NIE: [
-        "",
-        [
-          Validators.required,
-          Validators.pattern(
-            /((([X-Z])|([LM])){1}([-]?)((\d){7})([-]?)([a-zA-Z]{1}))/
-          ),
+    this.personalDataForm = this.formBuilder.group(
+      {
+        nombre: ["", [Validators.required]],
+        apellidos: ["", [Validators.required]],
+        edad: ["", [Validators.required, this.ageValidator]],
+        sexo: ["", [Validators.required]],
+        DNI: [
+          "",
+          [
+            Validators.required,
+            Validators.pattern(/((\d{8})([-]?)([a-zA-Z]))/),
+          ],
         ],
-      ],
-      CIF: [
-        "",
-        [
-          Validators.required,
-          Validators.pattern(/[a-zA-Z]{1}\d{7}[a-zA-Z0-9]{1}/),
+        NIE: [
+          "",
+          [
+            Validators.required,
+            Validators.pattern(
+              /((([X-Z])|([LM])){1}([-]?)((\d){7})([-]?)([a-zA-Z]{1}))/
+            ),
+          ],
         ],
-      ],
-      email: ["", [Validators.required, Validators.email]],
-      telefono: [
-        "",
-        [
-          Validators.required,
-          Validators.pattern(
-            /^\+?([0-9]{2})\)?[-. ]?([0-9]{3})[-. ]?([0-9]{3})[-. ]?([0-9]{3})$/
-          ),
+        CIF: [
+          "",
+          [
+            Validators.required,
+            Validators.pattern(/[a-zA-Z]{1}\d{7}[a-zA-Z0-9]{1}/),
+          ],
         ],
-      ],
-      url: [
-        "",
-        [
-          Validators.required,
-          Validators.pattern(
-            /[(http(s)?):\/\/(www\.)?a-zA-Z0-9@:%._\+~#=]{2,256}\.[a-z]{2,6}\b([-a-zA-Z0-9@:%_\+.~#?&//=]*)/
-          ),
+        email: ["", [Validators.required, Validators.email]],
+        telefono: [
+          "",
+          [
+            Validators.required,
+            Validators.pattern(
+              /^\+?([0-9]{2})\)?[-. ]?([0-9]{3})[-. ]?([0-9]{3})[-. ]?([0-9]{3})$/
+            ),
+          ],
         ],
-      ],
-      password: ["", [Validators.required]],
-      confirmacionPass: ["", [Validators.required]],
-      company: ["", [Validators.required]],
-      tarjeta: [
-        "",
-        [
-          Validators.required,
-          Validators.pattern(/^([0-9]{4}( |\-)){3}[0-4]{4}$/),
+        url: [
+          "",
+          [
+            Validators.required,
+            Validators.pattern(
+              /[(http(s)?):\/\/(www\.)?a-zA-Z0-9@:%._\+~#=]{2,256}\.[a-z]{2,6}\b([-a-zA-Z0-9@:%_\+.~#?&//=]*)/
+            ),
+          ],
         ],
-      ],
-      aceptedTerms: ["", Validators.required, Validators.requiredTrue],
-    });
+        password: ["", [Validators.required]],
+        confirmacionPass: ["", [Validators.required]],
+        company: ["", [Validators.required]],
+        tarjeta: [
+          "",
+          [
+            Validators.required,
+            Validators.pattern(/^([0-9]{4}( |\-)){3}[0-4]{4}$/),
+          ],
+        ],
+        aceptedTerms: ["", Validators.required, Validators.requiredTrue],
+      },
+      { validator: this.MustMatch("password", "confirmacionPass") }
+    );
   }
 
   ageValidator(age) {
@@ -89,9 +96,23 @@ export class PostFormComponent {
       return null;
     }
   }
-  checkPasswords(group: FormGroup) {
-    const pass = group.controls.password.value;
-    const confirmPass = group.controls.confirmacionPass.value;
-    return pass === confirmPass ? null : { notSame: true };
+
+  MustMatch(controlName: string, matchingControlName: string) {
+    return (formGroup: FormGroup) => {
+      const control = formGroup.controls[controlName];
+      const matchingControl = formGroup.controls[matchingControlName];
+
+      if (matchingControl.errors && !matchingControl.errors.mustMatch) {
+        // return if another validator has already found an error on the matchingControl
+        return;
+      }
+
+      // set error on matchingControl if validation fails
+      if (control.value !== matchingControl.value) {
+        matchingControl.setErrors({ mustMatch: true });
+      } else {
+        matchingControl.setErrors(null);
+      }
+    };
   }
 }
